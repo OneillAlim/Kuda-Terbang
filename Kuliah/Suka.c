6fdfdfe77a4408c7d6e6125264eb6441ed06b048
OneillAlim <oneill.alim@gmail.com>
2025-09-07 15:38:22 +0700

    Suka

---
+++ Kuliah/Suka.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char nama[] = "Bryan O'neill Alim";
+    char suka[] = "makan";
+    char hobi[] = "main voli gaming";
+    char bahasa[] = "C";
+
+    printf("Hai nama saya %s, saya suka %s dan hobi saya %s dengan bahasa favorite saya %s.\n",
+           nama, suka, hobi, bahasa);
+
+    return 0;
+
+}
