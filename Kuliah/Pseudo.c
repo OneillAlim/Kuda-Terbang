04cfdbd1b99ed4cc651e1fcf388a5b0537458d10
OneillAlim <oneill.alim@gmail.com>
2025-09-07 15:22:15 +0700

    perubahan

---
+++ Kuliah/Pseudo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+    //Pseudo code untuk menghitung luas persegi panjang
+    /* 
+    Start
+    Input Width, Length
+    
+    */
+int main(){
+    
+    printf("ini.c");
+
+    return 0;
+}
